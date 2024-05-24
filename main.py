8bdf21c3188bdaed480cca1fe4529bb3233021a4
EvilCrake <48925014+EvilCrake@users.noreply.github.com>
2024-05-24 17:13:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+a = ('Hello')
+bb = input('Enter ur name: ')
+
+def test(a, b):
+    a = ('Hello, ')
+    b = {bb}
+    print(a, bb)
+
+test(a, bb)
+
+aa = int(input('How are u? Answer: 1- Nice, 2- Sad, 3- So-so.'))
+ab = ('Nice!')
+ad = ('No more sadness!')
+ac = ('Its gonna be okay!')
+if aa == 1:
+    print(ab)
+if aa == 2:
+    print(ad)
+if aa == 3:
+    print(ac)
+def test2(aa, ab, ac):
+    print(aa, ab, ac)
+
+test2(aa, ab, ac)
+print('Good bye!')
